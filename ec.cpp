df9e41307dc8c30ba43b332a9125610adc55b347
Mohannad <mohannadmostafa@outlook.com>
2018-01-16 19:46:42 -0500

    Impelement add function to add two points

--- ec.cpp
+++ ec.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 #include "ec.hpp"
+#include "point.hpp"
 
 using namespace std;
 
@@ -23,14 +24,26 @@ ec::~ec()
 
 // Scalar multiplication returns a point X = kP
 Point scalarMultiply(int k, Point P);
+
 // Returns A + B
-Point point_addition(Point A, Point B);
-// Returns A + A 
-Point point_doubling(Point A);
+Point add(Point A, Point B){
+    Point P;
+    // get all x and y values of points A and B
+    int x1 = A.getX();
+    int y1 = A.getY();
+    int x2 = B.getX();
+    int y2 = B.getY();
+    // get the slope of two points
+    int slope = findSlope(x1, y1, x2, y2);
+
+    int x3 = slope^2 - x1 - x2;
+    int y3 = slope*(x1 - x3) - y1;
+
+}
 
 
 
-int findSlope(int x1, int x2, int y1, int y2) {
+int findSlope(int x1, int y1, int x2, int y2) {
     int slope, ydiff, xdiff;
     // if the two points are equal, slope is for point doubling
     // else slope is for point addition operation.
