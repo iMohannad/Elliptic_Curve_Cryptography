c5b83f73e570831c2a4c09a7ed765862ae49014f
Mohannad <mohannadmostafa@outlook.com>
2018-01-16 19:30:16 -0500

    Adding header file for elliptic curve

---
+++ ec.hpp
@@ -0,0 +1,23 @@
+class ec
+{
+public:
+    ec();
+    ec(int a, int b);
+    // Find all points in the elliptic curve defined
+    void calculatePoints();
+    // Scalar multiplication returns a point X = kP
+    Point scalarMultiply(int k, Point P);
+    // Returns A + B
+    Point point_addition(Point A, Point B);
+    // Returns A + A 
+    Point point_doubling(Point A);
+    ec &operator=(ec &&) = default;
+    ec &operator=(const ec &) = default;
+    ~ec();
+
+private:
+    int a, b; // points for elliptic curve equation
+    int M; // module
+    // Calculate the slope of two points
+    int findSlope(int x1, int x2, int y1, int y2);
+};
