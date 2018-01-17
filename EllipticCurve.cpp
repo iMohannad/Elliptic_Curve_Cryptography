2fc9f053d54abcc5d7e5571d829d0d13a9ff2299
Mohannad <mohannadmostafa@outlook.com>
2018-01-17 10:49:01 -0500

    add a check condition to make the point positive if it's still negative after reduction

--- EllipticCurve.cpp
+++ EllipticCurve.cpp
@@ -33,13 +33,23 @@ Point EllipticCurve::add(Point A, Point B){
     int y2 = B.getY();
     // get the slope of two points
     int slope = findSlope(x1, y1, x2, y2);
+    // cout << "Slope > " << slope << endl;
 
-    int x3 = slope^2 - x1 - x2;
-    int y3 = slope*(x1 - x3) - y1;
+    int x3 = (slope*slope - x1 - x2) % M;
+    // cout << "x3 > " << x3 << endl;
+    if (x3 < 0) {
+        x3 = (x3 + M) % M;
+    }
+
+    int y3 = (slope*(x1 - x3) - y1) % M;
+    // cout << "y3 > " << y3 << endl;
+    if (y3 < 0) {
+        y3 = (y3 + M) % M;
+    }
 
     Point P(x3, y3);
 
-    return P; 
+    return P;
 
 }
 
@@ -50,10 +60,11 @@ int EllipticCurve::findSlope(int x1, int y1, int x2, int y2) {
     // if the two points are equal, slope is for point doubling
     // else slope is for point addition operation.
     if ((x1 == x2) && (y1 == y2)) {
-        slope = ((3*x1*x1) + this->a)/(2*y1) % this->M;
+        slope = (((3*x1*x1) + this->a)/(2*y1)) % this->M;
     } else {
         ydiff = y2 - y1;
         xdiff = x2 - x1;
         slope = (ydiff / xdiff) % this->M;
     }
+    return slope;
 }
