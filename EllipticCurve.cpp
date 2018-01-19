b07a2d771065b2fa2217da63a73a5f80370e3170
Mohannad <mohannadmostafa@outlook.com>
2018-01-18 19:51:01 -0500

    Adding and removing comments

--- EllipticCurve.cpp
+++ EllipticCurve.cpp
@@ -14,9 +14,6 @@ EllipticCurve::EllipticCurve()
 }
 
 EllipticCurve::EllipticCurve(int a, int b, int M) : a(a), b(b), M(M) {
-    // this->a = a;
-    // this->b = b;
-    // this->M = M;
 }
 
 EllipticCurve::~EllipticCurve()
@@ -27,13 +24,9 @@ EllipticCurve::~EllipticCurve()
 Point EllipticCurve::scalarMultiply(int k, Point P){
     Point Q; // Initalize a point to (0,0)
     Point R = P;
-    // cout << "Q =  " << Q << endl;
-    // cout << "R =  " << R << endl;
     while (k != 0) {
         if (k & 1) {
-            // cout << "R + Q = " << R << " + " << Q  << " = ";
             Q = add(Q, R);
-            // cout << Q << endl;
         }
         R = add(R, R);
         k = k >> 1; // shift k one bit to the right
@@ -77,22 +70,35 @@ Point EllipticCurve::add(Point A, Point B){
 }
 
 
-
+/* This function finds the slope of two points
+ * if the two points are equal, it means point doubling,
+ * otherwise, it's point addition.
+ * Point doubling: slope = (3*x1*x1 + a)/2*y1 % M
+ * Point addition: slope = (y2-y1)/(x2-x1) % M
+ */
 int * EllipticCurve::findSlope(int x1, int y1, int x2, int y2) {
     int *slope; // initialize a pointer to slope
     int numerator, denominator;
     // if the two points are equal, slope is for point doubling
     // else slope is for point addition operation.
+
+    // ------------ POINT DOUBLING --------------
     if ((x1 == x2) && (y1 == y2)) {
         numerator = modulo((3*x1*x1) + this->a, M);
         denominator = modulo(2*y1, M);
-    } else {
+    }
+    // ------------ Point Addition --------------
+    else {
         numerator = modulo(y2-y1, M);
         denominator = modulo(x2-x1, M);
     }
     if (denominator == 0) {
         return NULL;
     }
+    /* since slope is modular division, if the numerator and denominator
+     * are not coprime, we need to multiply the numerator by the modular 
+     * inverse of denominator.
+     */
     int inv = inverseMod(denominator, this->M);
     *slope = inv*numerator % this->M;
     return slope;
