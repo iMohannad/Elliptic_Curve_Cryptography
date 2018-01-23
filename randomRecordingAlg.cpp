ce96de919d8e14b84091a212503363c3233dcef4
Mohannad <mohannadmostafa@outlook.com>
2018-01-23 01:10:11 -0500

    add recordingAlg file with pw function

---
+++ randomRecordingAlg.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+
+int getWMax(int k, int Wn, int * D, int size) {
+    w = Wn + 2; // Upper bound of w
+    int * Dw_complement;
+    for (int i=Wn+2; i > 0; i--) {
+        // Get Dw_complement
+
+    }
+}
+
+// pw(k) = k % (2**w)
+int pw(int k, int w) {
+    return k % (int) pow(2, w);
+}
