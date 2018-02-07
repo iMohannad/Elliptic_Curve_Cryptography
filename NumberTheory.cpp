6dd121146fbcea8e1fb4437c4095d62c300c9a91
Mohannad <mohannadmostafa@outlook.com>
2018-02-06 19:35:59 -0500

    change position of modulo function and use modulo function instead of using % operator

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -3,21 +3,21 @@
 namespace Cryptography {
 
 
+    int modulo (int m, int n) {
+        return m >= 0 ? m % n : (n - abs( m%n )) % n;
+    }
+
     /* Find the inverse of a number x mod n such that
      * x*i mod n = 1 where i is the inverse of x mod n.
      */
     int inverseMod(int x, int n) {
         int t; // a temp value
         for (int i = 0; i < n; i++) {
-            t = x*i % n;
+            t = modulo(x*i, n);
             if (t == 1) {
                 return i;
             }
         }
-    }
-
-    int modulo (int m, int n) {
-        return m >= 0 ? m % n : (n - abs( m%n )) % n;
     }
 
 }
